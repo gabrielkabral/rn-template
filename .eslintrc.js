4bda05fcd43ce9b3baa9d66b4f761e798ccebd27
Gabriel Menezes <gabriel@popcode.com.br>
2020-11-22 16:12:37 -0300

    setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier', "prettier/@typescript-eslint"],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  env: {
+    jest: true,
+    'jest/globals': true
+  },
   plugins: ['@typescript-eslint']
 }
