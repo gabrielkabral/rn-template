f66e6541ab3e91e495d10ed7341ac7f5398d834b
Gabriel Menezes <gabriel@popcode.com.br>
2020-11-22 15:52:59 -0300

    add react-native project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  semi: false,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 120,
+  tabWidth: 2
+}
